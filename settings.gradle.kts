1168c8e722fae61dcde3471e0ca59e5c25a32523
hipolito <huaylla7@hotmail.com>
2023-08-07 19:58:53 -0400

    Proyecto creado

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ei8 Razas Perros"
+include(":app")
+ 
